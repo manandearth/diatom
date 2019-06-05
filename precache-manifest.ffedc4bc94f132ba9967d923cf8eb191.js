5eee6d2f0334323ae2687dc2244803154620f7dd
manandearth <adamgefen@posteo.net>
2019-06-05 17:31:02 +0200

    Updates

---
+++ precache-manifest.ffedc4bc94f132ba9967d923cf8eb191.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f4c6883d45eed36a8548a04cea2edd",
+    "url": "/diatom/index.html"
+  },
+  {
+    "revision": "d716ba4c7da5f0c81a7c",
+    "url": "/diatom/static/css/main.35b1be93.chunk.css"
+  },
+  {
+    "revision": "b60b422bb4806f81faf5",
+    "url": "/diatom/static/js/2.d4c4656d.chunk.js"
+  },
+  {
+    "revision": "d716ba4c7da5f0c81a7c",
+    "url": "/diatom/static/js/main.42f506c4.chunk.js"
+  },
+  {
+    "revision": "afb17b7b340fb6abd171",
+    "url": "/diatom/static/js/runtime~main.9c965165.js"
+  }
+]);
